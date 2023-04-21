68392b6a61d7920615624d3059d0f2a0a2a4553c
ypf2021 <1472809818@qq.com>
2023-04-21 14:32:38 +0800

    docs: 添加注释，格式化文件

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,7 @@
 // 序列化存储 && 添加过期时间
 class Storage {
-    constructor(name) {
-        this.name = 'storage';
+    constructor(name = "storage") {
+        this.name = name;
     }
 
     /**
@@ -15,8 +15,10 @@ class Storage {
         let obj = {
             key: key,
             value: value
-        }
-        expires && (obj.expires = expires) && (obj.$startTime = new Date().getTime())
+        };
+        expires &&
+            (obj.expires = expires) &&
+            (obj.$startTime = new Date().getTime());
         if (obj.expires) {
             //如果obj.expires设置了的话
             //以obj.key为key，obj为值放进去
@@ -25,10 +27,10 @@ class Storage {
             //如果options.expires没有设置，就判断一下value的类型
             let type = Object.prototype.toString.call(obj.value);
             //如果value是对象或者数组对象的类型，就先用JSON.stringify转一下，再存进去
-            if (type == '[object Object]') {
+            if (type == "[object Object]") {
                 obj.value = JSON.stringify(obj.value);
             }
-            if (type == '[object Array]') {
+            if (type == "[object Array]") {
                 obj.value = JSON.stringify(obj.value);
             }
             localStorage.setItem(obj.key, obj.value);
@@ -40,7 +42,7 @@ class Storage {
         let item = localStorage.getItem(key);
         if (!item) {
             // 没有值时 item为null
-            return false
+            return false;
         }
 
         //先将拿到的试着进行json转为对象的形式
@@ -77,4 +79,4 @@ class Storage {
     }
 }
 
-export default new Storage()
+export default new Storage();
--- vue.config.js
+++ vue.config.js
@@ -1,87 +1,78 @@
-const path = require('path');
-const { merge } = require('webpack-merge')
+const path = require("path");
+const { merge } = require("webpack-merge");
 
 function resolve(dir) {
-  console.log("resolve", path.join(__dirname, '.', dir))
-  return path.join(__dirname, '.', dir);
+    console.log("resolve", path.join(__dirname, ".", dir));
+    return path.join(__dirname, ".", dir);
 }
 
 module.exports = {
-  publicPath: './',
-  assetsDir: 'static',
+    publicPath: "./",
+    assetsDir: "static",
 
-  css: {
-    loaderOptions: {
-      scss: {
-        additionalData: `@import "./src/theme/scss/index.scss";`,
-      },
+    css: {
+        loaderOptions: {
+            // 添加一些全局的scss配置
+            scss: {
+                additionalData: `@import "./src/theme/scss/index.scss";`
+            }
+        }
     },
-  },
 
-  chainWebpack: config => {
-    // 没写后缀时 只匹配 js 和 vue
-    config.resolve.extensions
-      .clear()
-      .add('.js')
-      .add('.vue')
+    chainWebpack: (config) => {
+        // 没写后缀时 只匹配 js 和 vue
+        config.resolve.extensions.clear().add(".js").add(".vue");
 
-    // 去掉原本的svg处理
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/assets/icon'))
-      .end();
-    // 加上 svg封装组件的loader
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/assets/icon'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      });
+        // 去掉原本的svg处理
+        config.module.rule("svg").exclude.add(resolve("src/assets/icon")).end();
+        // 加上 svg封装组件的loader
+        config.module
+            .rule("icons")
+            .test(/\.svg$/)
+            .include.add(resolve("src/assets/icon"))
+            .end()
+            .use("svg-sprite-loader")
+            .loader("svg-sprite-loader")
+            .options({
+                symbolId: "icon-[name]"
+            });
 
-    // 添加小图片转为base64(不设置时默认会将小于8kb进行base64转换)
-    config.module
-      .rule('images')
-      .parser({
-        dataUrlCondition: { maxSize: 10 * 1024 }
-      })
+        // 添加小图片转为base64(不设置时默认会将小于10kb进行base64转换)
+        config.module.rule("images").parser({
+            dataUrlCondition: { maxSize: 10 * 1024 }
+        });
 
-    // 避免重复引入，去除node_modules
-    config.module
-      .rule('js')
-      .exclude
-      .add(/node_modules/)
-      .end()
-      .use('babel-loader')
-      .tap(options => merge(options, {
-        plugins: ["@babel/plugin-transform-runtime"]
-      }))
+        // 避免重复引入，去除node_modules
+        config.module
+            .rule("js")
+            .exclude.add(/node_modules/)
+            .end()
+            .use("babel-loader")
+            .tap((options) =>
+                merge(options, {
+                    plugins: ["@babel/plugin-transform-runtime"]
+                })
+            );
 
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].cache = true
-        args[0].cacheLoaction = resolve('/node_modules/.cache/eslintcache')
-        return args
-      })
-
-  },
-  devServer: {
-    proxy: {
-      //配置跨域
-      '/api': {
-        target: 'http://localhost:8888', //这里后台的地址模拟的;应该填写你们真实的后台接口
-        changOrigin: true, //允许跨域
-        pathRewrite: {
-          /* 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/core/getData/userInfo 时
+        config.plugin("html").tap((args) => {
+            args[0].cache = true;
+            args[0].cacheLoaction = resolve("/node_modules/.cache/eslintcache");
+            return args;
+        });
+    },
+    devServer: {
+        proxy: {
+            //配置跨域
+            "/api": {
+                target: "http://localhost:8888", //这里后台的地址模拟的;应该填写你们真实的后台接口
+                changOrigin: true, //允许跨域
+                pathRewrite: {
+                    /* 重写路径，当我们在浏览器中看到请求的地址为：http://localhost:8080/api/core/getData/userInfo 时
             实际上访问的地址是：http://121.121.67.254:8185/core/getData/userInfo,因为重写了 /api
            */
-          '^/api': '',
-        },
-      },
-    },
-  },
-}
+                    "^/api": ""
+                }
+            }
+        }
+    }
+};
